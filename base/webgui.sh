466ebf4161fd85d3911da2328c444d56b5ff5be4
We1eVen <wsc50362@gmail.com>
2024-08-08 14:08:05 +0800

    Modified webgui.sh

--- base/webgui.sh
+++ base/webgui.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-tools/gui/websocketd --port=65430 --maxforks=1 --staticdir=tools/gui ./stairspeedtest /rpc
+if [ "$THREAD" != "4" ]; then
+   sed -i "s/thread_count=4/thread_count=$THREAD/g" pref.ini
+fi
+tools/gui/websocketd --port=$PORT --maxforks=$MAXFORKS --staticdir=tools/gui ./stairspeedtest /rpc
